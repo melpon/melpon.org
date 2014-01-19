1fb9a208b2647dbdb7e77cf45557d54d7b34630f
melpon <melt@e-ml.jp>
2014-01-19 16:53:36 +0900

    change title.

--- site/Handler/Header.hs
+++ site/Handler/Header.hs
@@ -2,11 +2,13 @@ module Handler.Header where
 
 import Import
 import qualified Yesod                                  as Y
+import qualified Data.List                              as List
 
 import Settings (widgetFile)
 import Foundation (Widget, App, Route(HomeR, AboutmeR))
 
-type Headers = [(String, Y.Route App, String)]
+type Header = (String, Y.Route App, String)
+type Headers = [Header]
 
 headers :: Headers
 headers =
@@ -14,9 +16,14 @@ headers =
   , ("aboutme", AboutmeR, "About Me")
   ]
 
+getPageTitle :: String -> Maybe String
+getPageTitle name =
+    (\(_, _, title) -> title) <$> List.find (\(name', _, _) -> name == name') headers
+
 getHeader :: String -> Widget
 getHeader name = do
-    Y.setTitle "melpon's Homepage"
+    let pageTitle = maybe (fail "name not found") id $ getPageTitle name
+    Y.setTitle $ Y.toHtml $ pageTitle ++ " - Licensed by Meatware"
     Y.addScriptRemote "//codeorigin.jquery.com/jquery-1.10.2.min.js"
     Y.addScriptRemote "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"
     Y.addStylesheetRemote "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"
--- site/Handler/Home.hs
+++ site/Handler/Home.hs
@@ -11,8 +11,4 @@ import Handler.Header(getHeader)
 getHomeR :: Handler Y.Html
 getHomeR = do
     Y.defaultLayout $ do
-        Y.setTitle "melpon's Homepage"
-        Y.addScriptRemote "//codeorigin.jquery.com/jquery-1.10.2.min.js"
-        Y.addScriptRemote "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"
-        Y.addStylesheetRemote "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"
         $(widgetFile "homepage")
