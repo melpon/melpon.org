05d1aa44a8b263f371d3c8c5b328ebc3f3b7e8f9
melpon <melt@e-ml.jp>
2014-02-19 00:40:01 +0900

    implementing cpprefjp slide

--- site/Handler/Publication/Cpprefjp.hs
+++ site/Handler/Publication/Cpprefjp.hs
@@ -6,13 +6,32 @@ import Import
 import qualified Yesod                                  as Y
 
 import Settings (widgetFile)
-import Foundation (Handler, Route(StaticR))
+import Foundation (Handler, Widget, Route(StaticR))
 import Settings.StaticFiles
   ( publication_io_2012_slides_js_require_1_0_8_min_js
   , publication_img_cpprefjp_to_github_png
   , publication_img_github_to_cpprefjp_png
+  , publication_img_cpprefjp_ui_png
+  , publication_img_cpprefjp_comtg1_png
+  , publication_img_cpprefjp_comtg2_png
   )
-import Handler.Publication.Slide (defaultLayout, withDefaultIO)
+import Handler.Publication.Slide (getTitleWidget, indexToWidget, Index(..), Title(..), defaultLayout, withDefaultIO)
+
+indexData :: Index
+indexData = Index "目次"
+  [ TitleOnly ("overview", "cpprefjpの概要")
+  , TitleOnly ("problem", "Google Sitesの問題点")
+  , Title ("do", "GitHubへの移行")
+      [ TitleOnly ("1st", "Google SitesからGitHubへ(@cpp_akira)")
+      , TitleOnly ("2nd", "GitHubからGoogle Sitesへ(@melponn)")
+      ]
+  ]
+
+index :: Maybe String -> Widget
+index = indexToWidget indexData
+
+title :: String -> Widget
+title = getTitleWidget indexData
 
 getPCpprefjpR :: Handler Y.Html
 getPCpprefjpR = do
