be93f344db5f0aa67e9b54120da1844f8b5ab653
melpon <melt@e-ml.jp>
2015-11-25 20:52:28 +0900

    update ghwebhook.sh

--- ghwebhook.sh
+++ ghwebhook.sh
@@ -6,15 +6,18 @@ su - melpon-org -c '
 set -ex
 
 cd melpon-org/
+git clean -xdqf
 git checkout master
+git clean -xdqf
 git pull
 
-git submodule update -i
-
-cd site
-rm -r dist/
-cabal-dev install
+cd site2
+./autogen.sh
+./configure --prefix=/usr/local/melpon-org --with-cppcms=/usr/local/cppcms --with-cppdb=/usr/local/cppdb
+make
 '
+cd /home/melpon-org/site2
+make install
 stop melpon-org || true
 sleep 1
 start melpon-org
